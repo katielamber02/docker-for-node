5a8e29061757442b51910887e375fe969bf69ab5
Katie Lamber02 <katielamber02@gmail.com>
2020-10-14 13:45:18 +0300

    healthchecks for depends-on

--- sample-nodemon/app.js
+++ sample-nodemon/app.js
@@ -34,6 +34,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
+  // added in order to restart nodemon
   res.status(err.status || 500);
   res.render('error');
 });
